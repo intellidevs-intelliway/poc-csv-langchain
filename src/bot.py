04b6c4a479a1953a1e47e33e1c91e12ddd50fe27
intellidevs-intelliway <112904469+intellidevs-intelliway@users.noreply.github.com>
2023-06-09 11:28:56 -0300

    Update bot.py to respond in pt-br

--- src/bot.py
+++ src/bot.py
@@ -15,8 +15,10 @@ class Bot:
         
     def translate(self, prompt: str, to: str, _from: str=None) -> str:
         llm = OpenAI(temperature=0)
-        full_query = f"""You are a translator bot who must simply translate the following input into {to}: {prompt}.
-        Do not translate important keywords that are relevant to the context.
+        full_query = f"""You are a translator bot who must simply translate the given input to {to}.
+        Do not translate important keywords that are relevant to the context, such as movie names, people and places. Do not translate proper nouns in general.
+        Now, translate this:
+        {prompt}.
         
         Your translation: """
         
@@ -26,11 +28,13 @@ class Bot:
     def run_agent(self, prompt: str, df: pd.DataFrame) -> str:
         # agent = create_csv_agent(OpenAI(temperature=0), files, verbose=True)
         agent_csv = create_pandas_dataframe_agent(OpenAI(temperature=0), df=df, verbose=True)
-        full_prompt = f"""Please provide the most complete answer possible to the following prompt:
-        {self.translate(prompt=prompt, to='en-us')}
-        
+        full_prompt = f"""
+        You are a curator bot who speaks Brazilian Portuguese and who will receive some knowledge and will retrieve all the information the user asks for in the most detailed way possible.
         No one-word responses, always form and return full sentences for your final answers.
-        Your responses must be returned in Brazilian Protuguese."""
+        Your responses must be returned in Brazilian Protuguese.
+        Please provide the most complete answer possible (in Brazilian Protuguese) to the following prompt:
+        {prompt}
+        """
         result = agent_csv.run(prompt)
         return result
 
@@ -45,7 +49,7 @@ class Bot:
             
             try:
                 response = str(self.run_agent(prompt=prompt, df=df))
-                return response
+                return self.translate(response, 'pt-br')
             except:
                 return "Something went wrong... try reformulating your question."
 
