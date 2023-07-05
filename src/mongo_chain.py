daa7636de00dbafcc20e1026a62899691740ea30
intellidevs-intelliway <112904469+intellidevs-intelliway@users.noreply.github.com>
2023-07-05 10:15:11 -0300

    Update mongo_chain.py

--- src/mongo_chain.py
+++ src/mongo_chain.py
@@ -8,8 +8,40 @@ load_dotenv()
 import os
 
 class MongoChain:
+    def __init__(self, connection_string: str, database: str, collection: str):
+        self.connection_string = str(connection_string).strip()
+        self.database = str(database).strip()
+        self.collection = str(collection).strip()
+    
     def detect_intent(self, query: str = "") -> str:
         llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k", verbose=True)
+        
+        python_repl = PythonREPL()
+        
+        cmd = f"""
+import subprocess
+import sys
+
+try:
+    import pymongo
+    from pymongo import MongoClient
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
+
+try:
+    client = MongoClient('{self.connection_string}')
+    db = client['{self.database}']
+    collection = db['{self.collection}']
+    
+    result = collection.find().limit(1)
+    
+    for document in result:
+        print(document)
+except Exception as e:
+    print(str(e))
+"""
+
+        sample = python_repl.run(cmd)
         
         template = """Given the following documentation and user input, extract from the input which method for collection is the most suitable one to do what the user wants and, if the user informs something that can be used as a query filter (id, name, etc), construct the correct command with the filter.
         Insert Documents:
@@ -63,23 +95,25 @@ class MongoChain:
         
         If the user refers to 'id', understand it as '_id'.
         
+        Take this collection object as sample as a means of understanding how this collection is structured: {sample}
+        
         This is the user's query: {input}
         
         Feel free to return the command that best fits the user's description, but you must only return the command.
         
         pymongo command: """
         
-        prompt = PromptTemplate(template=template, input_variables=['input'])
+        prompt = PromptTemplate(template=template, input_variables=['input', 'sample'])
         
         chain = LLMChain(llm=llm, prompt=prompt)
         
-        res = chain.run(input=query)
+        res = chain.run(input=query, sample=sample)
         
         # TODO -> get intent and run it through the next function (run_query), which will return the query results
         # run query results through llm with the user's initial prompt and return to the view
         
         # return res
-        query_res = self.run_query(self=MongoChain, connection_string=os.getenv("MONGO_CONNECTION_STRING"), command=res.strip(), input=query)
+        query_res = self.run_query(command=res.strip(), input=query)
         
         return query_res
     
@@ -103,59 +137,102 @@ class MongoChain:
         return res
         
     # def run_query(self, intention: str = 'find', connection_string: str = "", query = {}) -> str:
-    def run_query(self, connection_string: str = "", command: str = "", input: str = "") -> str:
+    def run_query(self, command: str = "", input: str = "") -> str:
         python_repl = PythonREPL()
         
         print(f"Command: {command}")
+        
+        cmd = f"""
+import subprocess
+import sys
 
-        cmd = textwrap.dedent(f"""
-        import subprocess
-        import sys
+try:
+    import pymongo
+    from pymongo import MongoClient
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
 
-        try:
-            import pymongo
-            from pymongo import MongoClient
-        except ModuleNotFoundError:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
-
-        try:
-            client = MongoClient('{connection_string}')
-            db = client['dummy']
-            collection = db['dummy-collection']
-
-            results = {command}
+try:
+    client = MongoClient('{self.connection_string}')
+    db = client['{self.database}']
+    collection = db['{self.collection}']
+"""
+        if "find" in command:
+            cmd = cmd + f"""    results = {command}
+    if isinstance(results, pymongo.cursor.Cursor):
+        results = list(results)
+    
+    if isinstance(results, list):
+        data = []
+        for res in results:
+            data.append(res)
+        print(data)
+    else:
+        print(results)
+        
+    client.close()
+except Exception as e:
+    print(str(e))"""
             
-            if isinstance(results, pymongo.cursor.Cursor):
-                results = list(results)
-
-            if isinstance(results, list):
-                data = []
-                for res in results:
-                    data.append(res)
-                print(data)
-            else:
-                print(results)
-                
-            client.close()
-        except Exception as e:
-            print(str(e))
-        """)
-
-        # print = "print(1+1)"
+        elif "update" in command:
+            cmd = cmd + f"""    {command}
+    print('Update successful!')
+    client.close()
+except Exception as e:
+    print(str(e))"""
+    
+        elif "delete" in command:
+            cmd = cmd + f"""    {command}
+    print('Database entry deleted successfully!')
+    client.close()
+except Exception as e:
+    print(str(e))"""
+        
+        elif "insert" in command:
+            cmd = cmd + f"""    {command}
+    print('Database entry created successfully!')
+    client.close()
+except Exception as e:
+    print(str(e))"""
+        
+        print(cmd)
 
         res = python_repl.run(cmd)
         
-        temp = self.evaluate_response(self=MongoChain, evaluate=res, user_query=input)
+        print("Python result: ", res)
+        
+        temp = self.evaluate_response(evaluate=res, user_query=input)
         
         return temp
     
         # return res 
         
-if __name__ == '__main__':
-    # res = MongoChain.detect_intent(self=MongoChain, query="What's the price of Smarthpone 1?")
-    res = MongoChain.run_query(self=MongoChain, connection_string=os.getenv("MONGO_CONNECTION_STRING"), command="collection.find_one({'name': 'Smartphone 1'})", input="")
-    print(res)
-    
+# if __name__ == '__main__':
+#     chain = MongoChain(connection_string=os.getenv('MONGO_CONNECTION_STRING'), database='dummy', collection='dummy-collection')
+#     res = chain.detect_intent(query="show everything in this database.")
+#     # res = MongoChain.run_query(connection_string=os.getenv("MONGO_CONNECTION_STRING"), command="collection.find_one({'name': 'Smartphone 1'})", input="")
+#     print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import sys
 # from io import StringIO
 # from typing import Dict, Optional
