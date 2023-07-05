64a6083aca5e70e7dc5eeb2a2848c9aab8fa2edc
intellidevs-intelliway <112904469+intellidevs-intelliway@users.noreply.github.com>
2023-07-05 10:16:26 -0300

    Update sql_bot.py

--- src/sql_bot.py
+++ src/sql_bot.py
@@ -7,7 +7,7 @@ class Sql_Bot:
     def __init__(self):
         pass
     
-    def query_sql(self, user_input: str, db_url: str):
+    def query_sql(self, user_input: str, db_url: str, db_uri: str):
         template = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer in the same language as the one used in the input.
         Use the following format:
 
@@ -26,23 +26,33 @@ class Sql_Bot:
             input_variables=["dialect", "table_info", "input"], template=template
         )
 
-        db = SQLDatabase.from_uri(
-            f"sqlite:///{db_url}"
-        )
-
-        llm = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo-16k", verbose=True)
-        
-        try:
-            print("Attempting query...")
-            sql_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, prompt=prompt, verbose=True)
-            result = sql_chain.run(user_input)
-            bot = Bot()
-            return bot.translate(prompt=result, to='pt-br')
-        
-        except:
-            print("Attempting sequential query...")
-            sql_chain = SQLDatabaseSequentialChain.from_llm(llm=llm, database=db, verbose=True)
-            result = sql_chain.run(user_input)
-            bot = Bot()
-            return bot.translate(prompt=result, to='pt-br')
-    
+        try:    
+            if db_uri:
+                db = SQLDatabase.from_uri(
+                # f"sqlite:///{db_url}"
+                database_uri=db_uri
+            )
+            else:
+                db = SQLDatabase.from_uri(
+                database_uri=f"sqlite:///{db_url}"
+            )
+            
+            llm = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo-16k", verbose=True)
+            
+            try:
+                print("Attempting query...")
+                sql_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, prompt=prompt, verbose=True)
+                result = sql_chain.run(user_input)
+                bot = Bot()
+                return bot.translate(prompt=result, to='pt-br')
+            
+            except:
+                print("Attempting sequential query...")
+                sql_chain = SQLDatabaseSequentialChain.from_llm(llm=llm, database=db, verbose=True)
+                result = sql_chain.run(user_input)
+                bot = Bot()
+                return bot.translate(prompt=result, to='pt-br')
+            
+        except Exception as e:
+            print(e)
+            return 'Não consegui conectar ao banco de dados... verifique sua URI.'
